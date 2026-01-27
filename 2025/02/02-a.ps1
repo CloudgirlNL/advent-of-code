feff6af84303054234509051aaa22b7032e6a42a
Marieke <113343394+CloudgirlNL@users.noreply.github.com>
2026-01-27 10:57:25 +0100

    Update 02-a.ps1

--- 2025/02/02-a.ps1
+++ 2025/02/02-a.ps1
@@ -1,1 +1,31 @@
+#Import puzzle input.
+$path = ".\input.txt"
+$AoCInput = Get-Content -Path $path
 
+$ranges = $AoCInput -split ","
+$invalidIDs = 0
+
+foreach ($range in $ranges) {
+    $IDs = $range -split "-"
+    [long]$firstID = $IDs[0]
+    [long]$lastID = $IDs[1]
+
+    
+    for ($i = $firstID; $i -le $lastID; $i++) {
+        $iAsString = $i.ToString()
+        $stringLength = $iasString.Length
+        $evenNumber = $stringLength % 2 -eq 0
+
+        if ($evenNumber -eq $true) {
+            $halfLength = $stringLength / 2
+            $firstHalf = $iasString.Substring(0, $halfLength)
+            $secondHalf = $iasString.Substring(($halfLength), $halfLength)
+            if ($firstHalf -eq $secondHalf) {
+                $invalidIDS += $i
+            } 
+        }
+
+    }
+}
+
+$invalidIDs
